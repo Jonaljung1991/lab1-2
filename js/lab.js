e97de3dea5fd79d7bb5d0aefa301235bc514ed33
Elin Jakobsson <elin-jakobsson@hotmail.com>
2018-02-01 16:19:37 +0100

    time and messages

--- js/lab.js
+++ js/lab.js
@@ -9,22 +9,94 @@ var config = {
 };
 firebase.initializeApp(config);
 
-let db = firebase.database();
-let message = {  // exempel objekt
-	sender: 'David',
-  text: 'Hur mår du'
-  image: 'https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/68dd54ca-60cf-4ef7-898b-26d7cbe48ec7/10-dithering-opt.jpg',
-  likes: 3,
-  dislikes: 2,
-  time: '12.30'
-};
 
-
-
-
+  // project code
 
 window.addEventListener('load', everything);
 
 function everything(event){
-  console.log('I am working :)');
+
+  let db = firebase.database();
+
+  // all html elements
+  const htmlElement = {
+    sendBtn: document.getElementById('sendBtn'),
+    textInput: document.getElementById("textInput"),
+    showTime: document.getElementById("showTime"),
+    chatContainer: document.getElementById("container")
+
+  }
+
+  htmlElement.sendBtn.style.disabled = true;
+
+  // Message
+  let message = {
+  	sender: 'David',
+    text: '',
+    likes: 3,
+    dislikes: 2,
+    time: ''
+  };
+
+
+  // Time Element
+
+  function getTime(){
+    let time = new Date();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let h = checkTime(hours);
+    let m = checkTime(minutes);
+    let finalTime = `${h} : ${m}`;
+    console.log(typeof hours);
+
+    function checkTime(i) {
+      if (i < 10) {
+          i = '0' + i;
+          }
+          return i;
+      }
+
+    return finalTime;
+  }
+
+
+  console.log(getTime());
+  console.log(htmlElement.sendBtn);
+
+
+  htmlElement.textInput.addEventListener("change" , function(event){
+    htmlElement.sendBtn.disabled = false;
+    message.text = htmlElement.textInput.value;
+    message.time = getTime();
+  })
+  htmlElement.sendBtn.addEventListener("click" , function(event){
+
+    if(message.text !== ""){
+    db.ref('/messages').push(message);
+    /**/
+
+    db.ref("/messages").on("value", function (snapshot) {
+        let userData = snapshot.val();
+        let output = '';
+
+
+        for (let info in userData) {
+          console.log(`allData har en property som heter ${info}`);
+          console.log('värdet är:' , userData[info]);
+          str = userData[info];
+
+
+            console.log('userdata ', str.text);
+           output += `<div class = 'message-light'>
+                          <p>${str.text} <span>${str.time}</span></p>
+                        </div>`
+
+            }
+            htmlElement.chatContainer.innerHTML = output;
+      });
+    }
+  })
+
+
 }
